b8d56f1e02f57d8a52410c46589b4f13d24ff723
NErd11d2 <69277915+Blub11d2@users.noreply.github.com>
2024-11-03 17:17:13 -0500

    playable kitchen

--- Assets/Scripts/DoorV2.cs
+++ Assets/Scripts/DoorV2.cs
@@ -6,6 +6,7 @@ public class DoorV2 : InteractableObjects
 {
 
     bool IsOpen = false;
+    [SerializeField] NextScene manager;
 
     public override void OnInteraction()
     {
@@ -13,6 +14,7 @@ public class DoorV2 : InteractableObjects
         {
             //Load the right scene here
             print("LoadingV2! You did it!!!... again");
+            manager.DOTHETHING();
         }
     }
 
